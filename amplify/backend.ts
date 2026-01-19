6d34bcca363445ab1a96b0744f73b21f584c168c
Connor Robinson <connor.3.robinson@bt.com>
2026-01-19 14:26:36 +0000

    add a tag to backend resources to billing purposes

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,13 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
+import { Tags } from 'aws-cdk-lib';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const tags = Tags.of(backend.stack);
+// Tag for project for billing purposes
+tags.add('project', 'otjobber');
